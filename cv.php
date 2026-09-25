186066e1bbdfdb744bd9dd7405ac52a2fd9c6974
nesquikx014 <mojoArch@users.noreply.github.com>
2026-09-25 23:46:26 +0200

    cv bijna af

--- cv.php
+++ cv.php
@@ -26,6 +26,51 @@
                   <img src="./images/me.jpg" alt="Portret van Nazli Eroglu">
               </div>
           </div>
+          <div class="cv-skills">
+      <h2>VAARDIGHEDEN</h2>
+
+      <table>
+          <thead>
+              <tr>
+                  <th scope="col">Onderdeel</th>
+                  <th scope="col">Vaardigheden</th>
+              </tr>
+          </thead>
+
+          <tbody>
+              <tr>
+                  <th scope="row">Web</th>
+                  <td>PHP, JavaScript, MySQL, Docker, Linux, Laravel</td>
+              </tr>
+
+              <tr>
+                  <th scope="row">Interactive Systems Design</th>
+                  <td>
+                      Elektronica Prototyping<br>
+                      Data &amp; Netwerk Software
+                  </td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+
+  <div class="cv-github">
+      <a
+          href="https://github.com/mojoArch"
+          target="_blank"
+          rel="noopener noreferrer"
+      >
+          BEKIJK MIJN GITHUB ↗
+      </a>
+  </div>
+
+  <div class="cv-education">
+      <h2>OPLEIDING</h2>
+
+      <h3>Software Developer</h3>
+      <p>Mediacollege Amsterdam</p>
+      <p>2024 - 2028</p>
+  </div>
       </section>
   </main>
 
